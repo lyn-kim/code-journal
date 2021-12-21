a45e83db2b37526161f7a24b47beaaa5ea4f0808
Lyn Kim <lynkim216@outlook.com>
2021-12-21 15:15:28 -0800

    create function to take single journal entry and return DOM tree to match placeholder example

--- js/main.js
+++ js/main.js
@@ -28,3 +28,43 @@ function collectInput(event) {
   preview.src = defaultSrc;
   entryForm.reset();
 }
+
+// Define a function that takes a single journal entry object
+// and returns a DOM tree that matches one of the example entries in the HTML.
+
+function getEntry(journal) {
+  var listItem = document.createElement('li');
+  listItem.className = 'row single-entry';
+
+  var columnHalf = document.createElement('div');
+  columnHalf.className = 'column-half';
+  listItem.appendChild(columnHalf);
+
+  var entryImg = document.createElement('img');
+  entryImg.className = 'entry-image';
+  entryImg.src = journal.url;
+  columnHalf.appendChild(entryImg);
+
+  columnHalf = document.createElement('div');
+  columnHalf.className = 'column-half';
+  listItem.appendChild(columnHalf);
+
+  var entryTitle = document.createElement('h2');
+  var titleText = document.createTextNode(journal.title);
+  entryTitle.appendChild(titleText);
+  entryTitle.className = 'no-margin-top';
+  columnHalf.appendChild(entryTitle);
+
+  var entryText = document.createElement('p');
+  var noteInput = document.createTextNode(journal.notes);
+  entryText.appendChild(noteInput);
+  entryText.className = 'entry-text';
+  columnHalf.appendChild(entryText);
+
+  return listItem;
+}
+
+var singleEntry = document.querySelector('.list');
+for (var i = 0; i < data.entries.length; i++) {
+  singleEntry.appendChild(getEntry(data.entries[i]));
+}
