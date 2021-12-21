116627331de1cec4de2759d469a949a8059257c2
Lyn Kim <lynkim216@outlook.com>
2021-12-20 17:34:29 -0800

    Listen for 'input' events on the imgUrlInput to update the src attribute of the photo preview when the input value changes AND update outline-color of focus to match navbar

--- js/main.js
+++ js/main.js
@@ -1,2 +1,14 @@
 /* global data */
 /* exported data */
+
+// Listen for 'input' events on the photoUrl input
+// to update the src attribute of the photo preview
+// when the input value changes.
+
+var imageUrlInput = document.querySelector('#img-url');
+var preview = document.querySelector('#preview');
+imageUrlInput.addEventListener('input', displayImg);
+
+function displayImg(event) {
+  preview.src = event.target.value;
+}
